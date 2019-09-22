9a047f4c341c7803111bf0815eafb258cbd77961
henriklippke <h.lippke@exporo.com>
2019-09-22 21:52:07 +0200

    Add localPrerequisites script: install latest aws-cli

--- localPrerequisites/init.sh
+++ localPrerequisites/init.sh
@@ -1,6 +1,6 @@
 # AWS Credentials
-printf "[default]\naws_access_key_id = DEFAULT_ACCESS_KEY\naws_secret_access_key = DEFAULT_SECRET_KEY" > ~/.aws/credentials
-printf "[default]\nregion = eu-central-1" > ~/.aws/config
+printf "[default]\naws_access_key_id = DEFAULT_ACCESS_KEY\naws_secret_access_key = DEFAULT_SECRET_KEY" >> ~/.aws/credentials
+printf "[default]\nregion = eu-central-1" >> ~/.aws/config
 
 # DynamoDB
 aws dynamodb --endpoint-url http://dynamodb:8000 create-table --cli-input-json file://dynamoDbCacheTable.json --region eu-central-1
