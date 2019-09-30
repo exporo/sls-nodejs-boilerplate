d9ef457f3782bb1c4e319f2ada0b5453ecf3ee24
henriklippke <h.lippke@exporo.com>
2019-09-30 20:15:56 +0200

    SmokeTestController: change lambda tests

--- application/http/controllers/smokeTestController.ts
+++ application/http/controllers/smokeTestController.ts
@@ -2,9 +2,9 @@ import {exec} from 'child_process';
 
 exports.handler = (event, context, callback) => {
 
-    exec('./node_modules/mocha/bin/mocha -r ts-node/register ./tests/**/*.spec.ts --exit --reporter mocha-simple-html-reporter', (err, stdout, stderr) => {
+    exec('./node_modules/mocha/bin/mocha -r ts-node/register ./tests/**/*.spec.ts --exit', (err, stdout, stderr) => {
+        console.log(err, stdout, stderr);
         if (err) {
-            console.log(err);
             callback({
                 statusCode: 500,
                 headers: {
