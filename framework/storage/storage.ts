dd3a580d0bb1c9a9e6ced3a117ba0670539103a3
henriklippke <h.lippke@exporo.com>
2019-10-01 17:01:20 +0200

    feature/storage: initial commit

--- framework/storage/storage.ts
+++ framework/storage/storage.ts
@@ -1,27 +1,9 @@
-import * as S3FS from 's3fs';
-import * as fs from 'fs';
+import { StorageManager } from'@slynova/flydrive';
 
 const config = require('../../application/config/storage.ts');
-let storageProvider;
 
-switch (process.env.AWS_LAMBDA_FUNCTION_NAME ? 'aws' : 'docker') {
-    case 'docker': {
-        storageProvider = fs.promises;
-        break;
-    }
-    case 'aws': {
-        storageProvider = fs.promises;
-        //replace with https://github.com/Slynova-Org/flydrive
-        break;
-    }
-}
+// AWS_LAMBDA_FUNCTION_NAME
+//storage.disk('awsCloud');
+export const Storage = new StorageManager(config);
 
-export const Storage = storageProvider;
 
-export const storage_path = function (fileName) {
-    if (config.provider === 'local') {
-        return config.local.prefix + fileName;
-    }
-
-    return fileName;
-};
