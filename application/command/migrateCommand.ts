2b80c35b6db2389afd6b2ea16afeaa78fc055b01
henriklippke <h.lippke@exporo.com>
2019-10-01 22:08:10 +0200

    Update migrate command

--- application/command/migrateCommand.ts
+++ application/command/migrateCommand.ts
@@ -1,7 +1,14 @@
 import {exec} from 'child_process';
 
 exports.handler = (event, context, callback) => {
-    exec('npm run migrate:latest',(err, stdout, stderr) => {
-        console.log(err, stdout, stderr);
+    let out;
+
+    exec('npm run migrate:latest', (err, stdout, stderr) => {
+        out = stdout;
+    }).on('exit', (code) => {
+        if (code === 0) {
+            callback(null, out);
+        }
+        callback(out);
     });
 };
