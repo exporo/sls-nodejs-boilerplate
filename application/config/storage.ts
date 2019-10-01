faabf6f62a0e84abaa2a0bc0b1c0d07c9aa06cf0
henriklippke <h.lippke@exporo.com>
2019-10-01 15:30:05 +0200

    Update storage conf

--- application/config/storage.ts
+++ application/config/storage.ts
@@ -4,6 +4,7 @@ module.exports = {
         prefix: 'storage/'
     },
     s3: {
-        'bucket': 'exporo-sls-storage'
+        region: "eu-central-1",
+        bucket: process.env.AWS_BUCKET
     }
 };
