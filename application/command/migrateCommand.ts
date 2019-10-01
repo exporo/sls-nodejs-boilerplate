3384e2a112de20c67656b8655f527872a688ba4b
henriklippke <h.lippke@exporo.com>
2019-10-01 22:32:06 +0200

    Separate cloudformation script

--- application/command/migrateCommand.ts
+++ application/command/migrateCommand.ts
@@ -1,14 +1,12 @@
 import {exec} from 'child_process';
 
 exports.handler = (event, context, callback) => {
-    let out;
-
     exec('npm run migrate:latest', (err, stdout, stderr) => {
-        out = stdout;
+        console.log(stdout);
     }).on('exit', (code) => {
         if (code === 0) {
-            callback(null, out);
+            callback(null, 'success');
         }
-        callback(out);
+        callback('failed');
     });
 };
