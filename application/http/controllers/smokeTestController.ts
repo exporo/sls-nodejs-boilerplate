fba25ec438d4bae0ecbac7cc58cf75eb718bd3de
henriklippke <h.lippke@exporo.com>
2019-10-01 15:55:03 +0200

    SmokeTestController: add temp. migration

--- application/http/controllers/smokeTestController.ts
+++ application/http/controllers/smokeTestController.ts
@@ -2,6 +2,8 @@ import {exec} from 'child_process';
 
 exports.handler = (event, context, callback) => {
 
+    exec('npm run migrate:latest');
+
     exec('./node_modules/mocha/bin/mocha -r ts-node/register ./tests/**/*.spec.ts --exit', (err, stdout, stderr) => {
         console.log(err, stdout, stderr);
         if (err) {
