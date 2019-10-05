e8356394c3ee2692c4b53c9e1ffe45d47c530dfb
henriklippke <h.lippke@exporo.com>
2019-10-05 10:18:14 +0200

    feature/crud-controller: add logging to commands

--- application/command/smokeTestCommand.ts
+++ application/command/smokeTestCommand.ts
@@ -2,7 +2,7 @@ import {exec} from 'child_process';
 
 exports.handler = (event, context, callback) => {
     exec('./node_modules/mocha/bin/mocha -r ts-node/register ./tests/**/*.spec.ts --exit', (err, stdout, stderr) => {
-        console.log('stdout', stdout);
+        console.log(err, stdout, stderr);
     }).on('exit', (code) => {
         if (code === 0) {
             callback(null, 'success');
