d4a8900dcb765498cf9218de4a508c30800ab043
henriklippke <h.lippke@exporo.com>
2019-11-10 18:54:25 +0100

    Prototype curd creator

--- framework/command/crudCreator/crudCreator.js
+++ framework/command/crudCreator/crudCreator.js
@@ -1,94 +1,53 @@
-const inquirer = require('inquirer');
+var inquirer = require('inquirer');
 
-console.log('Hi, welcome to Node Pizza');
 
-var questions = [
+var baseQuestions = [
     {
-        type: 'confirm',
-        name: 'toBeDelivered',
-        message: 'Is this for delivery?',
-        default: false
+        type: 'input',
+        name: 'namespace',
+        message: 'The namespace (folder name) inside ./application/domain:',
     },
     {
         type: 'input',
-        name: 'phone',
-        message: "What's your phone number?",
-        validate: function(value) {
-            var pass = value.match(
-                /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
-            );
-            if (pass) {
-                return true;
-            }
-
-            return 'Please enter a valid phone number';
-        }
-    },
-    {
-        type: 'list',
-        name: 'size',
-        message: 'What size do you need?',
-        choices: ['Large', 'Medium', 'Small'],
-        filter: function(val) {
-            return val.toLowerCase();
-        }
-    },
-    {
-        type: 'input',
-        name: 'quantity',
-        message: 'How many do you need?',
-        validate: function(value) {
-            var valid = !isNaN(parseFloat(value));
-            return valid || 'Please enter a number';
-        },
-        filter: Number
-    },
-    {
-        type: 'expand',
-        name: 'toppings',
-        message: 'What about the toppings?',
-        choices: [
-            {
-                key: 'p',
-                name: 'Pepperoni and cheese',
-                value: 'PepperoniCheese'
-            },
-            {
-                key: 'a',
-                name: 'All dressed',
-                value: 'alldressed'
-            },
-            {
-                key: 'w',
-                name: 'Hawaiian',
-                value: 'hawaiian'
-            }
-        ]
-    },
-    {
-        type: 'rawlist',
-        name: 'beverage',
-        message: 'You also get a free 2L beverage',
-        choices: ['Pepsi', '7up', 'Coke']
-    },
-    {
-        type: 'input',
-        name: 'comments',
-        message: 'Any comments on your purchase experience?',
-        default: 'Nope, all good!'
-    },
-    {
-        type: 'list',
-        name: 'prize',
-        message: 'For leaving a comment, you get a freebie',
-        choices: ['cake', 'fries'],
-        when: function(answers) {
-            return answers.comments !== 'Nope, all good!';
-        }
+        name: 'name',
+        message: 'The name of your CURD-Controller:',
     }
 ];
 
-inquirer.prompt(questions).then(answers => {
-    console.log('\nOrder receipt:');
-    console.log(JSON.stringify(answers, null, '  '));
+var addFieldQuestion = [
+    {
+        type: 'confirm',
+        name: 'addFIelds',
+        message: 'Do u want to add a field: ',
+    }
+];
+
+var fieldQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'The name of the field: ',
+    },
+    {
+        type: 'rawlist',
+        name: 'fieldType',
+        message: 'The field type: ',
+        choices: ['string', 'integer', 'float', 'foreignKey']
+    },
+];
+
+inquirer.prompt(baseQuestions).then(base => {
+    var fields = [];
+
+    while (addField()) {
+        var field = inquirer.prompt(fieldQuestions);
+        fields.push(field);
+    }
 });
+
+
+async function addField() {
+    var field = await inquirer.prompt(addFieldQuestion);
+    return field;
+}
+
